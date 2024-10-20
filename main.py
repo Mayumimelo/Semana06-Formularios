a0dcb257bef277cc151c2e12a8cca50fc611cf9d
mayumi <mayumi.melo@aluno.ifsp.edu.br>
2024-10-20 22:21:25 +0000

    first commit

--- main.py
+++ main.py
@@ -13,9 +13,17 @@ moment = Moment(app)
 def index():
      return render_template('index.html', current_time=datetime.utcnow())
 
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
+@app.route('/user/<name>/<prontuario>/<instituicao>')
+def user(name, prontuario, instituicao):
+    return render_template('user.html', name=name, prontuario=prontuario, instituicao=instituicao)
+
+@app.route('/contexto_requisicao/<name>')
+def contexto_requisicao(name, navegador=None, ip=None, host=None):
+    navegador = request.user_agent.browser
+    ip = request.remote_addr
+    host = request.host
+
+    return render_template('contexto_requisicao.html', name=name, navegador=navegador, ip=ip, host=host)
 
 @app.errorhandler(404)
 def page_not_found(e):
